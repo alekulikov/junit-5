8919adeaf50f105ddca5cb037ade160ce7eac286
alekulikov <aleksandr.a.kulikov@mvideo.ru>
2024-11-17 21:07:15 +0300

    hw11-jenkins | add allure attaches

--- src/test/java/qa/guru/tests/TestBase.java
+++ src/test/java/qa/guru/tests/TestBase.java
@@ -6,8 +6,12 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static qa.guru.helpers.AttachHelper.*;
 
 public class TestBase {
 
@@ -17,6 +21,12 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
@@ -26,6 +36,10 @@ public class TestBase {
 
     @AfterEach
     void attachesAndCloseBrowser() {
+        screenshotAs("Result");
+        pageSource();
+        browserConsoleLogs();
+        addVideo();
         closeWebDriver();
     }
 }

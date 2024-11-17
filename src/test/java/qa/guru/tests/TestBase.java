096ab8bf89f8f7c08d9a2b56b9b7a9f794557e38
alekulikov <aleksandr.a.kulikov@mvideo.ru>
2024-11-17 19:30:32 +0300

    hw11-jenkins | add selenoid

--- src/test/java/qa/guru/tests/TestBase.java
+++ src/test/java/qa/guru/tests/TestBase.java
@@ -1,7 +1,10 @@
 package qa.guru.tests;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
@@ -10,5 +13,11 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void attachesAndCloseBrowser() {
+        closeWebDriver();
     }
 }
